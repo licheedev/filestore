03dafe0986389ea5aff0f8a249dffbf512d97153
licheedev <licheedev@foxmail.com>
2019-08-28 13:21:25 +0800

    upload

---
+++ fq/init-goagent-php.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+# 下载goagent-php相关文件到服务器中
+wget --no-check-certificate https://raw.githubusercontent.com/licheetec/filestore/master/fq/goagent-php/index.js
+wget --no-check-certificate https://raw.githubusercontent.com/licheetec/filestore/master/fq/goagent-php/index.php
+wget --no-check-certificate https://raw.githubusercontent.com/licheetec/filestore/master/fq/goagent-php/index.py
+wget --no-check-certificate https://raw.githubusercontent.com/licheetec/filestore/master/fq/goagent-php/relay.php
